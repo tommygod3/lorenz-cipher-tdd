481cf6073b88c4233841f85d3e8377d9b1b330d1
tommygod3 <tommygod1@live.com>
2020-03-31 22:54:22 +0100

    Added first real test

--- test/Spec.hs
+++ test/Spec.hs
@@ -9,6 +9,13 @@ import Lorenz
 
 -- tests
 
+prop_encodeCharInRange :: Char -> Bool
+prop_encodeCharInRange x = 
+    0 <= encodeChar x <= 94
+
+prop_encodeChar :: Char -> Bool
+prop_encodeChar x = 
+    chr (encodeChar x) == chr ((ord x) - 32)
 
 -- main
 return []
