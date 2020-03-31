b8ac59d5093271958f1aa77a9c167541b779eb65
tommygod3 <tommygod1@live.com>
2020-04-01 00:16:42 +0100

    Completed implementation to pass tests

--- src/Lorenz.hs
+++ src/Lorenz.hs
@@ -9,3 +9,8 @@ encodeChar x | ord x < 32 = 95
 encodeChar x | ord x > 126 = 95
 encodeChar x = (ord x) - 32
 
+decodeChar :: Int -> Char
+decodeChar x | x < 0 = chr (127)
+decodeChar x | x > 94 = chr (127)
+decodeChar x = chr (x + 32)
+
