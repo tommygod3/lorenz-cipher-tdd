17b0a1a89c6940257e1d27b86003d56fccb639df
tommygod3 <tommygod1@live.com>
2020-03-31 15:13:44 +0100

    Added test structure and example test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
+module Main where
+
+-- imports
+
+import Test.QuickCheck
+
+-- tests
+
+prop_reverseLength :: [Int] -> Bool
+prop_reverseLength xs = 
+    length xs == length (reverse xs)
+
+-- main
+return []
+main = $quickCheckAll
