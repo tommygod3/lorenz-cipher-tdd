37f08d12d3ceca56f5d925161494889b2c425558
tommygod3 <tommygod1@live.com>
2020-03-31 23:36:34 +0100

    Improved encodeChar comment

--- src/Lorenz.hs
+++ src/Lorenz.hs
@@ -2,8 +2,8 @@ module Lorenz where
 
 import Data.Char
 
--- Shift ASCII characters to be Int between 0 and 94
--- All other non alphabetical ASCII characters go to 95 (\DEL)
+-- Shift printing ASCII characters (32-126) to be Int between 0 and 94
+-- All other non printing ASCII characters go to 95 (\DEL)
 encodeChar :: Char -> Int
 encodeChar x | ord x < 32 = 95
 encodeChar x | ord x > 126 = 95
