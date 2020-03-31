8757e4d8c048cc2d02b33e80a3381c5ce0c1078d
tommygod3 <tommygod1@live.com>
2020-03-31 23:45:34 +0100

    Added decodeChar tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -10,6 +10,7 @@ import Lorenz
 
 -- tests
 
+-- encodeChar
 prop_encodeCharInRange :: Char -> Bool
 prop_encodeCharInRange x = 
     encodeChar x <= 95
@@ -23,6 +24,26 @@ prop_encodeChar x =
     chr (encodeChar x) == 
         if (32 <= ord x) && (ord x <= 126)
             then chr ((ord x) - 32) 
+        else chr 95
+
+-- decodeChar
+prop_decodeCharInRange :: Int -> Bool
+prop_decodeCharInRange x = 
+    ord (decodeChar x) <= 95
+
+prop_decodeCharPositive :: Int -> Bool
+prop_decodeCharPositive x = 
+    0 <= ord (decodeChar x)
+
+prop_decodeCharIsAscii :: Int -> Bool
+prop_decodeCharIsAscii x = 
+    isAscii (decodeChar x)
+
+prop_decodeChar :: Int -> Bool
+prop_decodeChar x = 
+    decodeChar x == 
+        if (32 <= x) && (x <= 126)
+            then chr (x - 32)
         else chr 95
 
 -- main
